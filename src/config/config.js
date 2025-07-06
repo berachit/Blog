389535b896086007a9141c9c69b2b6a7471be2e5
berachit <154239767+berachit@users.noreply.github.com>
2025-07-06 13:59:19 +0530

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
-    appWriteUrl: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
-    projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appWriteUrl: String(import.meta.env.VITE_ENDPOINT),
+    projectId: String(import.meta.env.VITE_PROJECT_ID),
+    collectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    bucketId: String(import.meta.env.VITE_BUCKET_ID)
 }
